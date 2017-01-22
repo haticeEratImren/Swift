e1558f84fe6efde509bc2ec0aeec15bd05108ab8
Hatice Erat <haticeerat@gmail.com>
2017-01-22 19:45:40 +0000

    tmp

--- PickImage/PickImage/ViewController.swift
+++ PickImage/PickImage/ViewController.swift
@@ -8,20 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var ImagePickerView: UIImageView!
     @IBOutlet weak var albumButton: UIBarButtonItem!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField! { didSet { topTextField.delegate = self } }
+    @IBOutlet weak var bottomTextField: UITextField! { didSet { bottomTextField.delegate = self } }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        topTextField.text = "TOP"
+        topTextField.textAlignment = NSTextAlignment.center
+        bottomTextField.text = "BOTTOM"
+        bottomTextField.textAlignment = NSTextAlignment.center
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeToKeyboardNotifications()
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,6 +63,49 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             dismiss(animated: true, completion: nil)
         }
     }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if (textField.text == "TOP" ||  textField.text == "BOTTOM") {
+            textField.text = ""
+        }
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+ 
+    }
+    
+    func keyboardWillShow(_ notification:Notification){
+        view.frame.origin.y -= getKeyboardHeight(notification)
+
+    }
+    
+ 
+    func keyboardWillHide(_ notification:Notification){
+        view.frame.origin.y = 0
+        
+    }
+    
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    
     
 }
 
