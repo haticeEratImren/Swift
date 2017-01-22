5218732c92146ff856964979db16ec7c98b805bd
Hatice Erat <haticeerat@gmail.com>
2017-01-22 15:40:02 +0000

    pick an image from photos

---
+++ PickImage/PickImage/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  PickImage
+//
+//  Created by Hatice Erat on 22/01/2017.
+//  Copyright © 2017 Hatice Imren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var ImagePickerView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func pickAnImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController, animated: true, completion: nil)
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage]
+        
+        if ((image as? UIImage) != nil) {
+            ImagePickerView.image = image as! UIImage?
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+}
+
